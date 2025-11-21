45ae4afe9d4c1bb790d1121dacc319796a9c9d3f
Ravi <ravikumarv7639@gmail.com>
2025-11-21 11:48:39 +0530

    Add files via upload

---
+++ 8051_Microcontroller/1.Led/led_blinking.c
@@ -0,0 +1,22 @@
+#include<reg51.h>
+
+void delay(int a)
+{
+	int i,j;
+	for(i=0; i<a; i++)
+	{
+		for(j=0; j<250; j++)
+		{
+		}
+	}
+}
+
+void main(){
+	while(1)
+	{
+		P2 = 0X01; // 1.085us
+		delay(200);
+		P2 = 0x00; // 1.085us 
+		delay(200);
+	}
+}
